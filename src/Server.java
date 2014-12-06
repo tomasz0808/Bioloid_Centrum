1faaf1b7078a3906650184b274e025a9fffa6cf8
Dominik P. <pettke.dominik@gmail.com>
2014-12-06 03:31:49 +0100

    Final z 5.12

--- src/Server.java
+++ src/Server.java
@@ -20,96 +20,86 @@ public class Server {
 	public static Socket socket = null;
 	public static DataInputStream dataInputStream = null;
 	public static DataOutputStream dataOutputStream = null;
-	public JButton button;
+	public JButton wakeUp;
 	public String dataIn;
 	public String dataOut;
-	public boolean isConnected =false;
-	public boolean isWaiting =false;
+	public boolean isConnected = false;
+	
 	private JButton connectToRobot;
 	private JLabel connectionStatus;
 	private int port;
 	private int connectionPort= 10006;
-	private boolean connButtClick=false;
-
-	
 	
  public Server() throws IOException 
  {    
-	 JFrame okno = Gui2.frame;
- 	 button = Gui2.wakeUp;
- 	 connectToRobot= Gui2.connectToRobot;
-	 connectionStatus = Gui2.connectionStatus;
-	 
-	 			connectToRobot.addActionListener(new ActionListener() {
+	JFrame okno = Gui2.frame;
+ 	wakeUp = Gui2.wakeUp;
+ 	connectToRobot= Gui2.connectToRobot;
+	connectionStatus = Gui2.connectionStatus;
+	connectToRobot.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			connecion();
+		}});
+	
+	while(isConnected ==true){							
+		while(socket.isConnected()){    
+			try{
+				
+				dataInputStream = new DataInputStream(socket.getInputStream());
+				dataOutputStream = new DataOutputStream(socket.getOutputStream());
+				wakeUp.addActionListener(new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e) {
-							isWaiting = true;
-							connecion();
-					}});
-				        	
-							while(isWaiting == true)
-							{	
-								System.out.println("1step");
-								if(isConnected ==true)
-								{
-									System.out.println("2step");
-									if(socket.isConnected())
-										System.out.println("3step");
-									{	
-								 		while(socket.isConnected()){    
-								 			try{
-								 				
-								 				 	dataInputStream = new DataInputStream(socket.getInputStream());
-								 				 	dataOutputStream = new DataOutputStream(socket.getOutputStream());
-								 			 	}catch (IOException e1) 
-								 					{
-								 			 			e1.printStackTrace();
-								 					}
-										}
-									}	
-								}
-							}
-
-				 
-	 	
-	 okno.addWindowListener(new WindowAdapter() {	
+						try {
+							dataOutputStream.writeUTF("siema");
+							System.out.println("wysla³em");
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+				});	
+			}
+			catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}									
+	}
+				 	 	
+	okno.addWindowListener(new WindowAdapter() {	
 		@Override
 		public void windowClosing(WindowEvent e) {
-			super.windowClosing(e);
+		super.windowClosing(e);
 			if(isConnected == true){
 				try {				
 					if(Server.socket.isConnected()){
-						dataInputStream.close();
-						dataOutputStream.close();
-						socket.close();		
-						
+//						dataInputStream.close();
+						//dataOutputStream.close();
+						socket.close();			
 					}
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
+				} 
+				catch (IOException e1) {
 						e1.printStackTrace();
 				}			
 			}
-			}	
-		});
+		}	
+	});
  }
  
- 
+
  public void connectionAlert(boolean isConnected)
  {
 	 if(isConnected == false){		
-			 connectionStatus.setForeground(Color.RED);
-			 connectionStatus.setText("Not Connected");
-			 connectToRobot.setText("Connect to Robot");		 		  
+		 connectionStatus.setForeground(Color.RED);
+		 connectionStatus.setText("Not Connected");
+		 connectToRobot.setText("Connect to Robot");		 		  
 	 }else{
-		 
 		 connectionStatus.setForeground(new Color(60, 179, 113));
 		 connectionStatus.setText("Connected on: "  + port);
 		 connectToRobot.setText("Abort Connection");
-	 }
-	 
+	 } 
  }
- 
- 
  
  //--------------------- Connection --------------------------------------
  public void connecion() {
@@ -122,47 +112,27 @@ public class Server {
  //--------------------- THREAD ------------------------------------------
  Thread t = new Thread(new Runnable()  
  {  
+	 
+ 
      public void run()  
      {  
          try  
          {  
-
         	serverSocket = new ServerSocket(connectionPort);
           	port = serverSocket.getLocalPort();
             socket = serverSocket.accept(); // Tu czeka
-           
             isConnected = true; 
             connectionAlert(isConnected);
-
             connectToRobot.setEnabled(true);
-            
-            
          }  
          catch(IOException ie)  
          {  
              //  
          } 
-           
-     }  
+     } 
+     
  });
  //----------------------------------------------------------------------
- 
- 
- 
- public static synchronized Socket getSocket()
- {
-	 return socket;
- }
- public static synchronized DataInputStream getDataInputStream()
- {
-	 return dataInputStream;
- }
- public static synchronized OutputStream getDataOutputStream()
- {
-	 return dataOutputStream;
- }
- 
- 
 }
 
 // dataIn = dataInputStream.readUTF();
