2beceb7e9aaf68af7a6aff531bee960ab8ba3848
Dominik P. <pettke.dominik@gmail.com>
2014-12-09 23:37:10 +0100

    Prawie final.

--- src/Gui2.java
+++ src/Gui2.java
@@ -8,7 +8,6 @@ import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -27,81 +26,38 @@ import javax.swing.table.DefaultTableModel;
 
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 
-
 public class Gui2 {
 
 	public static JFrame frame = new JFrame();
 	private JPanel contentPane;
 	public static JTable table;
-	
-	public static DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+
+	public static DateFormat df = new SimpleDateFormat("dd/MM/yy      HH:mm:ss");
 	public static int tableId = 1;
-	private static  DefaultTableModel model = new DefaultTableModel(
-			new Object[][] {		},
-			new String[] {"Id", "Time", "Patient status", "Assistance"});
+	private static DefaultTableModel model = new DefaultTableModel(
+			new Object[][] {}, new String[] { "Id", "Time", "Patient status" });
 	public static JButton wakeUp;
-	public static JButton connectToRobot;	
+	public static JButton connectToRobot;
 	public static JLabel connectionStatus;
 	public static JButton btnSendText;
 	public static JTextField sendText;
-	private String ipAddr ;
+	public static JLabel robotStatus;
+	private String ipAddr;
 	public static JButton clearLogs;
-	
-	
+	static int[] helpRows = new int[1000];
 
-	
-	 
-	
-	public void dodaj(){
-		for(int i=0; i<6; i++){
-			
-			Date dateobj = new Date();
-			//table.setDefaultRenderer(Object.class, new MyRenderer());
-			table.getColumn("Patient status").setCellRenderer(
-			        new DefaultTableCellRenderer() {
-
-			        Color originalColor = null;
-
-			        @Override
-			        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-			            DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-			            if (originalColor == null) {
-			                originalColor = getForeground();
-			            }
-			            if (value == null) {
-			                renderer.setText("");
-			            } else {
-			                renderer.setText(value.toString());
-			            }
-
-			            if (value.toString().equalsIgnoreCase("Help")) {
-			                renderer.setBackground(Color.RED);
-			            } else {
-			                renderer.setForeground(originalColor); // Retore original color
-			            }
-			            return renderer;
-			        }
-			    });
-			model.insertRow(0, new Object [] {tableId, df.format(dateobj),"siema", "iza"});
-			tableId++;		
-		}
-	}
-	
-
-	
-	
 	public Gui2() throws UnknownHostException {
 		frameinit();
-		dodaj();
+
 	}
-	
-	public void ipGetter() throws UnknownHostException{
-		
+
+	public void ipGetter() throws UnknownHostException {
+
 		ipAddr = InetAddress.getLocalHost().getHostAddress();
 	}
-	
-	public void frameinit() throws UnknownHostException{
-		
+
+	public void frameinit() throws UnknownHostException {
+
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(100, 100, 800, 650);
@@ -110,47 +66,49 @@ public class Gui2 {
 		frame.setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		
-		
-		
 		JPanel panelIp = new JPanel();
-		panelIp.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelIp.setBorder(new TitledBorder(null, "", TitledBorder.LEADING,
+				TitledBorder.TOP, null, null));
 		panelIp.setBounds(20, 30, 480, 130);
 		contentPane.add(panelIp);
 		panelIp.setLayout(null);
-		
+
 		JLabel lblConnectionStatus = new JLabel("Connection status:");
 		lblConnectionStatus.setBounds(217, 77, 106, 16);
 		panelIp.add(lblConnectionStatus);
-				
+
 		connectionStatus = new JLabel("Not conneted");
 		connectionStatus.setForeground(Color.RED);
 		connectionStatus.setBounds(331, 71, 160, 28);
 		panelIp.add(connectionStatus);
-		
-		connectToRobot = new JButton("Connect to robot"); 	
+
+		connectToRobot = new JButton("Connect to robot");
 		connectToRobot.setBounds(217, 13, 143, 44);
 		panelIp.add(connectToRobot);
-		
+
 		ipGetter();
 		JLabel myIp = new JLabel(ipAddr);
-		myIp.setFont(myIp.getFont().deriveFont(myIp.getFont().getStyle() | Font.BOLD));
+		myIp.setFont(myIp.getFont().deriveFont(
+				myIp.getFont().getStyle() | Font.BOLD));
 		myIp.setBounds(64, 76, 107, 19);
 		panelIp.add(myIp);
-		
-		JLabel lblIp = new JLabel("<html><p>Exchange your ip addres with<br>\r\nrobot to communicate</P>\r\n<br><br>\r\n\r\n<p>Your ip:</p>\r\n</html>");
+
+		JLabel lblIp = new JLabel(
+				"<html><p>Exchange your ip addres with<br>\r\nrobot to communicate</P>\r\n<br><br>\r\n\r\n<p>Your ip:</p>\r\n</html>");
 		lblIp.setFont(UIManager.getFont("CheckBox.font"));
 		lblIp.setVerticalAlignment(SwingConstants.TOP);
 		lblIp.setBounds(12, 13, 177, 79);
 		panelIp.add(lblIp);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setViewportBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		scrollPane.setViewportBorder(new TitledBorder(null, "",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		scrollPane.setBounds(20, 340, 755, 250);
 		contentPane.add(scrollPane);
-		
-		
+
 		table = new JTable();
+		table.setShowVerticalLines(false);
+		table.setGridColor(Color.GRAY);
 		table.setEnabled(true);
 		table.setFont(UIManager.getFont("Table.font"));
 		table.setSurrendersFocusOnKeystroke(true);
@@ -159,41 +117,44 @@ public class Gui2 {
 		table.setCellSelectionEnabled(true);
 		table.setColumnSelectionAllowed(true);
 		table.setModel(model);
-		table.getColumnModel().getColumn(0).setPreferredWidth(52);
-		table.getColumnModel().getColumn(1).setPreferredWidth(112);
-		table.getColumnModel().getColumn(2).setPreferredWidth(295);
+		table.getColumnModel().getColumn(0).setPreferredWidth(40);
+		table.getColumnModel().getColumn(1).setPreferredWidth(200);
+		table.getColumnModel().getColumn(2).setPreferredWidth(800);
 		scrollPane.setViewportView(table);
-				
+
 		clearLogs = new JButton("Clear logs");
 		clearLogs.setBounds(20, 300, 100, 25);
 		contentPane.add(clearLogs);
-		
+
 		JPanel panelWakeUp = new JPanel();
-		panelWakeUp.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelWakeUp.setBorder(new TitledBorder(null, "", TitledBorder.LEADING,
+				TitledBorder.TOP, null, null));
 		panelWakeUp.setBounds(20, 180, 250, 100);
 		contentPane.add(panelWakeUp);
-		
+
 		wakeUp = new JButton("Wake up Robot");
 		wakeUp.setBounds(15, 15, 140, 45);
-		
+
 		JLabel lblRobotStatus = new JLabel("Status:");
 		lblRobotStatus.setBounds(15, 70, 50, 15);
-		
-		JLabel robotStatus = new JLabel("Sleeping");
-		robotStatus.setBounds(65, 70, 60, 15);
+
+		robotStatus = new JLabel("Not started");
+		robotStatus.setForeground(Color.RED);
+		robotStatus.setBounds(65, 70, 100, 15);
 		panelWakeUp.setLayout(null);
 		panelWakeUp.add(wakeUp);
 		panelWakeUp.add(lblRobotStatus);
 		panelWakeUp.add(robotStatus);
-		
+
 		JPanel panelSendText = new JPanel();
-		panelSendText.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelSendText.setBorder(new TitledBorder(null, "",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panelSendText.setBounds(295, 180, 480, 100);
 		contentPane.add(panelSendText);
-		
+
 		btnSendText = new JButton("Send Message");
 		btnSendText.setBounds(15, 15, 140, 45);
-		
+
 		sendText = new JTextField();
 		sendText.setHorizontalAlignment(SwingConstants.CENTER);
 		sendText.setText("Write your text here...");
@@ -202,59 +163,71 @@ public class Gui2 {
 		panelSendText.setLayout(null);
 		panelSendText.add(btnSendText);
 		panelSendText.add(sendText);
-		
-		JLabel logo = DefaultComponentFactory.getInstance().createLabel("New JGoodies label");
+
+		JLabel logo = DefaultComponentFactory.getInstance().createLabel(
+				"New JGoodies label");
 		logo.setIcon(new ImageIcon(Gui2.class.getResource("/cross/cross.png")));
 		logo.setBounds(550, 30, 159, 130);
 		contentPane.add(logo);
 		frame.setVisible(true);
-		
-		clearLogs.addActionListener(new ActionListener() {	
+
+		clearLogs.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				model.setRowCount(0);
 				tableId = 1;
 			}
 		});
-		
+
 	}
-	
 
+	public static void addtotable(String status) {
 
-	
-	public static void addtotable(String status, String nurse){
 		
+		//
 		table.getColumn("Patient status").setCellRenderer(
-		        new DefaultTableCellRenderer() {
+				new DefaultTableCellRenderer() {
 
-		        Color originalColor = null;
+					Color originalColor = null;
 
-		        @Override
-		        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-		            DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-		            if (originalColor == null) {
-		                originalColor = getForeground();
-		            }
-		            if (value == null) {
-		                renderer.setText("");
-		            } else {
-		                renderer.setText(value.toString());
-		            }
+					@Override
+					public Component getTableCellRendererComponent(
+							JTable table, Object value, boolean isSelected,
+							boolean hasFocus, int row, int column) {
+						DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) super
+								.getTableCellRendererComponent(table, value,
+										isSelected, hasFocus, row, column);
+						if (value == null) {
+							renderer.setText("");
+						} else {
+							renderer.setText(value.toString());
+						}
 
-		            if (value.toString().equalsIgnoreCase("Help")) {
-		                renderer.setBackground(Color.RED);
-		            } else {
-		                renderer.setForeground(originalColor); // Retore original color
-		            }
-		            return renderer;
-		        }
-		    });
+						if (value.toString().toLowerCase().contains("alert") ) {
+							renderer.setBackground(Color.RED);
+							renderer.setForeground(Color.WHITE);
+						} else if (value.toString().contains("Request")|| value.toString().toLowerCase().contains("connecion lost")) {
+							renderer.setBackground(Color.ORANGE);
+							renderer.setForeground(Color.BLACK);
+						}
+						 else if (value.toString().contains("started")) {
+							renderer.setBackground(Color.BLUE);
+							renderer.setForeground(Color.WHITE);
+						}
+
+						else {
+							renderer.setForeground(Color.BLACK);
+							renderer.setBackground(Color.WHITE);
+
+						}
+						return renderer;
+					}
+				});
+
+		
 		Date dateobj = new Date();
-		model.insertRow(0, new Object [] {tableId, df.format(dateobj),status, nurse});
+		model.insertRow(0, new Object[] { tableId, df.format(dateobj),
+				" " + status });
 		tableId++;
 	}
-	
-	
-	
-
 }
--- src/Server.java
+++ src/Server.java
@@ -8,233 +8,235 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTable;
 import javax.swing.JTextField;
+
+
 public class Server {
 
-	public static ServerSocket serverSocket = null;
-	public static Socket socket = null;
-	public static DataInputStream dataInputStream = null;
-	public static DataOutputStream dataOutputStream = null;
-	public JButton wakeUp;
+	static ServerSocket serverSocket = null;
+	static Socket socket = null;
+	static DataInputStream dataInputStream = null;
+	static DataOutputStream dataOutputStream = null;
+	private JButton wakeUp;
 	private String dataIn;
-	private String dataOut;
-	public boolean isConnected = false;
-	
+	private boolean isConnected = false;
+	private int isWake = 0;
+
 	private JButton connectToRobot;
 	private JLabel connectionStatus;
 	private JButton btnSendMsg;
 	private int port;
-	private int connectionPort= 10006;
+	private int connectionPort = 10006;
 	private JTextField sendText;
-	public boolean connBool = false;
+	private boolean connBool = false;
+	private JLabel robotStatus;
 
+	public Server() throws IOException {
+		JFrame okno = Gui2.frame;
+		wakeUp = Gui2.wakeUp;
+		connectToRobot = Gui2.connectToRobot;
+		connectionStatus = Gui2.connectionStatus;
+		btnSendMsg = Gui2.btnSendText;
+		sendText = Gui2.sendText;
+		robotStatus = Gui2.robotStatus;
+		btnSendMsg.setEnabled(false);
+		wakeUp.setEnabled(false);
 
-	
- public Server() throws IOException 
- {    
-	JFrame okno = Gui2.frame;
- 	wakeUp = Gui2.wakeUp;
- 	connectToRobot= Gui2.connectToRobot;
-	connectionStatus = Gui2.connectionStatus;
-	btnSendMsg = Gui2.btnSendText;
-	sendText = Gui2.sendText;
-	
-	
-	getTextToTable.start();
-	connThread.start(); 
-	connectToRobot.addActionListener(new ActionListener() {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(isConnected==true){
-				try {
-					isConnected = false;
-					serverSocket.close();
-					dataInputStream.close();
-					dataOutputStream.close();
-					socket.close();
-					connectionAlert(isConnected);
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			}else{
-				
+		getTextToTable.start();
+		connThread.start();
+		connectToRobot.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isConnected == true) {
+					try {
+						isWake = 0;
+						isConnected = false;
+						serverSocket.close();
+						dataInputStream.close();
+						dataOutputStream.close();
+						socket.close();
+						connectionAlert(isConnected, isWake);
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				} else {
+
 					try {
 						connecion();
 					} catch (IOException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-				
-				  
+
+				}
 			}
-		}});
-	wakeUp.addActionListener(new ActionListener() {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(isConnected == true){
-				try {
-					dataOutputStream.writeUTF("wake up");
-					
-					System.out.println("Siema");
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-			}else{
-				System.out.println("Ÿle");			}
-			
-		}
-	});
-	
-	btnSendMsg.addActionListener(new ActionListener() {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(isConnected == true){
-				try {
-					String rawText = sendText.getText();
-					dataOutputStream.writeUTF(rawText);
-					 
-					System.out.println("Siema");
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-			}else{
-				System.out.println("Ÿle");			}
-			
-		}
-	});
-	
-	
-	
-	
-	
-				 	 	
-	okno.addWindowListener(new WindowAdapter() {	
-		@Override
-		public void windowClosing(WindowEvent e) {
-		super.windowClosing(e);
-			if(isConnected == true){
-				try {				
-					if(Server.socket.isConnected()){
-						dataInputStream.close();
-						dataOutputStream.close();
-						socket.close();			
+		});
+//------------------ Wake up Action -----------------------------------
+		wakeUp.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isConnected == true) {
+					try {
+						dataOutputStream.writeUTF("wake up");
+						Gui2.addtotable("Robot is waking up...");
+					} 
+					catch (IOException e1) {
+						Gui2.addtotable("Error");
 					}
-				} 
-				catch (IOException e1) {
-						e1.printStackTrace();
-				}			
+				}
 			}
-		}	
+		});
+//------------------ Send Message Action ------------------------------
+		btnSendMsg.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isConnected == true) {
+					try {
+						String rawText = sendText.getText();
+						dataOutputStream.writeUTF(rawText);
+						Gui2.addtotable("Message: " + rawText + " send");
+					} catch (IOException e1) {
+						Gui2.addtotable("Error");
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+//------------------ Exit Action -------------------------------------
+		okno.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				super.windowClosing(e);
+				if (isConnected == true) {
+					try {
+						if (Server.socket.isConnected()) {
+							dataInputStream.close();
+							dataOutputStream.close();
+							socket.close();
+						}
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+	}
+
+//------------------ METHODS ---------------------------------------------------------
+//------------------ Status changer --------------------------------------------------
+	public void connectionAlert(boolean isConnected, int isWake) {
+		if (isConnected == false) {
+			connectToRobot.setEnabled(true);
+			connectionStatus.setForeground(Color.RED);
+			connectionStatus.setText("Not Connected");
+			connectToRobot.setText("Connect to Robot");
+		} else {
+			connectToRobot.setEnabled(true);
+			connectionStatus.setForeground(new Color(60, 179, 113));
+			connectionStatus.setText("Connected port: " + port);
+			connectToRobot.setText("Abort Connection");
+		}
+		if (isWake == 1) {
+			robotStatus.setForeground(new Color(60, 179, 113));
+			robotStatus.setText("Active");
+			btnSendMsg.setEnabled(false);
+			wakeUp.setEnabled(false);
+
+		} else if (isWake == 2) {
+			robotStatus.setForeground(Color.BLACK);
+			robotStatus.setText("Sleeping");
+			btnSendMsg.setEnabled(true);
+			wakeUp.setEnabled(true);
+
+		} else if (isWake == 0) {
+			robotStatus.setForeground(Color.RED);
+			robotStatus.setText("Not started");
+			btnSendMsg.setEnabled(false);
+			wakeUp.setEnabled(false);
+
+		}
+
+	}
+
+// --------------------- Connection -------------------------------------------------
+	public void connecion() throws IOException {
+		connectionStatus.setForeground(Color.BLUE);
+		connectionStatus.setText("Connecting...");
+		connectToRobot.setEnabled(false);
+		serverSocket = new ServerSocket(connectionPort);
+		port = serverSocket.getLocalPort();
+		connBool = true;
+
+		// onnThread.start();
+	}
+
+	// --------------------- THREAD ------------------------------------------
+	Thread connThread = new Thread(new Runnable() {
+		public void run() {
+			while (true) {
+				while (connBool == true) {
+					try {
+						serverSocket.setSoTimeout(10000);
+						socket = serverSocket.accept();
+
+						dataInputStream = new DataInputStream(
+								socket.getInputStream());
+						dataOutputStream = new DataOutputStream(
+								socket.getOutputStream());
+						isConnected = true;
+						isWake = 2;
+						connectionAlert(isConnected, isWake);
+
+						connBool = false;
+					} catch (IOException ie) {
+						connBool = false;
+						isConnected = false;
+
+						try {
+							serverSocket.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						connectionAlert(isConnected, isWake);
+					}
+				}
+			}
+		}
 	});
- }
- 
 
- public void connectionAlert(boolean isConnected)
- {
-	 if(isConnected == false){		
-		 connectToRobot.setEnabled(true);
-		 connectionStatus.setForeground(Color.RED);
-		 connectionStatus.setText("Not Connected");
-		 connectToRobot.setText("Connect to Robot");		 		  
-	 }else{
-		 connectToRobot.setEnabled(true);
-		 connectionStatus.setForeground(new Color(60, 179, 113));
-		 connectionStatus.setText("Connected on: "  + port);
-		 connectToRobot.setText("Abort Connection");
-	 } 
- }
- 
- //--------------------- Connection --------------------------------------
- public void connecion() throws IOException {
-	 connectionStatus.setForeground(Color.BLUE);
-	 connectionStatus.setText("Connecting...");
-	 connectToRobot.setEnabled(false);
-	 serverSocket = new ServerSocket(connectionPort);
-	 port = serverSocket.getLocalPort();
-	 connBool = true ;
-	
-
-	 //onnThread.start();  
- }
- 
- //--------------------- THREAD ------------------------------------------
- Thread connThread = new Thread(new Runnable()  
- {  
-    public void run()  
-    {  
-    	while(true){
-	    	 while(connBool == true){
-		         try  
-		         { 
-		        	serverSocket.setSoTimeout(3000);
-		            socket = serverSocket.accept();
-		            
-		            dataInputStream = new DataInputStream(socket.getInputStream());
-					dataOutputStream = new DataOutputStream(socket.getOutputStream());
-		            isConnected = true; 
-		            connectionAlert(isConnected);	           
-		            connBool = false;
-		         }  
-		         catch(IOException ie)  
-		         {
-		        	connBool = false;
-		        	isConnected = false;
-		        		try {
-		        			serverSocket.close();
-		        		}catch (IOException e){
-		        			e.printStackTrace();
-		        		}	
-		        	connectionAlert(isConnected);
-		         		} 
-	    	 }
-    	}
-    }
-}      
-);
- 
- Thread getTextToTable = new Thread(new Runnable()  
- {  
-    public void run()  
-    {  
-    	while(true){	
-	    	while(isConnected == true){
-		    	try {
-					dataIn = dataInputStream.readUTF();
-					if (!dataIn.isEmpty()){    	
-				    	Gui2.addtotable(dataIn, dataIn);
-				    	
-				    }
-					
-				} 
-		    	catch (IOException e) {
-		    		try {
-	        			serverSocket.close();
-	        		}catch (IOException e1){
-	        			e1.printStackTrace();
-	        		}
-		    		isConnected = false;
-		    		connectionAlert(isConnected);
-		    		break;
-					//getTextToTable.interrupt();
-				}	
-			}	
-    	}	
-    }
- }  
- );
- //----------------------------------------------------------------------
+	Thread getTextToTable = new Thread(new Runnable() {
+		public void run() {
+			while (true) {
+				while (isConnected == true) {
+					try {
+						dataIn = dataInputStream.readUTF();
+						if (!dataIn.isEmpty()) {
+							if (dataIn.equalsIgnoreCase("sleep")) {
+								isWake = 2;
+							} else if (dataIn.equalsIgnoreCase("wake")) {
+								isWake = 1;
+							} else
+								Gui2.addtotable(dataIn);
+						}
+						connectionAlert(isConnected, isWake);
+					} catch (IOException e) {
+						try {
+							serverSocket.close();
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+						Gui2.addtotable("Connecion Lost");
+						isConnected = false;
+						isWake = 0;
+						connectionAlert(isConnected, isWake);
+						break;
+					}
+				}
+			}
+		}
+	});
+// ----------------------------------------------------------------------
 }
-
-	       
-						   	
